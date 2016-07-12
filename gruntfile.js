4f0b8542aaccfd9facc189f136e4f0276ad6f5a5
FreyYan9 <Yan9_Furen@163.com>
2016-07-12 14:29:12 +0800

    第一个node项目, 差不多练习到这儿结束!

--- gruntfile.js
+++ gruntfile.js
@@ -36,6 +36,12 @@ module.exports = function(grunt){
 			},
 		},
 
+		mochaTest : {
+			options : {
+				reporter : "spec"
+			},
+			src : ['test/**/*.js']
+		},
 
 		concurrent : {
 			tasks : ["nodemon" , "watch"],
@@ -48,9 +54,11 @@ module.exports = function(grunt){
 	grunt.loadNpmTasks("grunt-contrib-watch");// 如果对文件有添加、修改、删除等操作，重新执行注册好的任务
 	grunt.loadNpmTasks("grunt-nodemon");//实时监听， 监听app.js，如果app.js有改动，会自动重启app.js
 	grunt.loadNpmTasks("grunt-concurrent");//针对慢任务， 如less 、sass
+	grunt.loadNpmTasks("grunt-mocha-test");//针对慢任务， 如less 、sass
 
 	grunt.option('force',true);//为了便于开发的时候不会因为语法的错误或者一些警告来中断grunt的整体任务
 
 	grunt.registerTask("default",["concurrent"]);
+	grunt.registerTask("test",["mochaTest"]);
 
 }
